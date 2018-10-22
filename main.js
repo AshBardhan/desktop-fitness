5b62dc14776c3583e9e7a4393091ebf5668fc066
ashish.bardhan <ashish.bardhan@wingify.com>
2018-10-22 08:55:44 +0530

    New Electron Version Upgrade and Minor UI Tweaks

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({width: 1280, height: 800})
 
     // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:8000/');
+    mainWindow.loadFile('index.html');
     isAppFocused = true;
 
     // Open the DevTools.
